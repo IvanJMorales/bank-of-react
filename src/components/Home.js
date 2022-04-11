3fdeeead53efe5d4f0c07463b3cf0c0fc89c8d99
Ivan Morales <IvanJohnMorales@Gmail.com>
2022-04-11 12:37:30 -0400

    Updated file

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <img src="https://picsum.photos/200/200" alt="bank" />
+                <h1>Bank of React</h1>
+            </div>
+        )
+    }
+}
+
+export default Home
