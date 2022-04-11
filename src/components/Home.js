d88b919a7de6cb401e45216ee7b57e57a417c07d
Ivan Morales <IvanJohnMorales@Gmail.com>
2022-04-11 18:41:02 -0400

    Added login and redirect

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,6 +10,7 @@ class Home extends Component {
                 <h1>Bank of React</h1>
 
                 <Link to="/userProfile">User Profile</Link>
+                <Link to="/login">Login</Link>
 
                 <AccountBalance accountBalance={this.props.accountBalance}/>
             </div>
@@ -17,4 +18,4 @@ class Home extends Component {
     }
 }
 
-export default Home
+export default Home;
